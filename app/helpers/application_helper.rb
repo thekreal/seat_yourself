e708d44a8a2e8e81565ab4f0d273260d0cfea0db
Scott Chu <domaru.tdot@gmail.com>
2014-02-07 13:32:14 -0500

    Created a home page and helper method titler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  SITE_NAME = "SEAT URSELF"
+
+  def titler(page_title)
+    page_title.empty? ? site_name : "#{site_name} | #{page_title}"
+  end
+
+  def site_name
+    return SITE_NAME
+  end
+
 end
