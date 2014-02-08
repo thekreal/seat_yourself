f71537166d2b4fbb412fdbde75e34ac55d468ed8
Kevin Freeman <kevinfreeman@rogers.com>
2014-02-08 15:31:48 -0500

    added some basic validations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
+
+  validates :time, presence: true
+  validates :number_of_people, numericality: {
+                                only_integer: true,
+                                greater_than_or_equal_to: 1
+                              }
 end
