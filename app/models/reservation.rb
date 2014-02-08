7f84aec2f371d6e1b13eef7d8152336aa9f7fe29
Kevin Freeman <kevinfreeman@rogers.com>
2014-02-08 18:29:21 -0500

    reservations now pick up the user_id

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,6 @@ class Reservation < ActiveRecord::Base
   validates :time, presence: true
   validates :number_of_people, numericality: {
                                 only_integer: true,
-                                greater_than_or_equal_to: 1
+                                less_than_or_equal_to: 100
                               }
 end
