128cbb2986f5117e9b337c55c934422aed8a7e90
Scott Chu <domaru.tdot@gmail.com>
2014-02-10 14:06:55 -0500

    Updated available_seats method

--- app/models/location.rb
+++ app/models/location.rb
@@ -12,8 +12,8 @@ class Location < ActiveRecord::Base
                                 message: "must be a valid number ( Greater or equal to 1)"
                               }
 
-  def available_seats
-    reserved_seats = reservations.select { |r| r.persisted? }.inject(0) { |sum, r| sum + r.number_of_people }
+  def available_seats(id)
+    reservations.select { |r| r.persisted? || r.id != id }.inject(0) { |sum, r| sum + r.number_of_people }
     return number_of_seats - reserved_seats
   end
 
